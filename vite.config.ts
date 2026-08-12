5d12127f7404ae0a951eba963ffc12a4f16922de
Hlioimport <helio.pedro.98@gmail.com>
2026-08-12 18:34:46 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // <--- Ajuste definitivo para carregar o visual (CSS) corretamente em subpastas
+  base: '/Agenda-Perpetua-/',
 })
