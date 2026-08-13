976ce4fcd76b6e4279d44afad42ef11f300ec03d
Hlioimport <helio.pedro.98@gmail.com>
2026-08-12 23:59:43 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Agenda-Perpeteua-/', // <--- Esta linha diz ao Vite exatamente onde o site está hospedado
+  base: '/Agenda-Perpetua-/',
 })
