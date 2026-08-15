b2e10ea952818cc247514da7a8acb1a64d6ef554
Hlioimport <helio.pedro.98@gmail.com>
2026-08-15 11:26:24 -0300

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,20 +1,14 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth, GoogleAuthProvider } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
 
-// Credenciais exatas do seu projeto Agenda Perpétua
 const firebaseConfig = {
-  apiKey: "AIzaSyAsb-V3YV78U2XmU_s7pE6b1WvA4YtZnc",
-  authDomain: "agenda-perpetua-a83d2.firebaseapp.com",
-  projectId: "agenda-perpetua-a83d2",
-  storageBucket: "agenda-perpetua-a83d2.appspot.com",
-  messagingSenderId: "1055849302114",
-  appId: "1:1055849302114:web:0a364fbfb7c25e41a832d3"
+  apiKey: "AIzaSyBwrOl3IFveoD6MXUZKHC_0sa0XohvL_Ds",
+  authDomain: "agenda-perpetua.firebaseapp.com",
+  projectId: "agenda-perpetua",
+  storageBucket: "agenda-perpetua.firebasestorage.app",
+  messagingSenderId: "1062998494461",
+  appId: "1:1062998494461:web:d817cfd39ebbd2b3450294"
 };
 
-// Inicializa o Firebase
 const app = initializeApp(firebaseConfig);
-
 export const auth = getAuth(app);
-export const googleProvider = new GoogleAuthProvider();
-export const db = getFirestore(app);
