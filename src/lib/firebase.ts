42ef7a1420e3ba267bde6ac8ca8316f2949f5a09
Hlioimport <helio.pedro.98@gmail.com>
2026-08-15 11:39:53 -0300

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
   apiKey: "AIzaSyBwrOl3IFveoD6MXUZKHC_0sa0XohvL_Ds",
@@ -10,5 +11,12 @@ const firebaseConfig = {
   appId: "1:1062998494461:web:d817cfd39ebbd2b3450294"
 };
 
+// Inicializa o app Firebase
 const app = initializeApp(firebaseConfig);
+
+// Exporta todos os módulos que o projeto precisa (auth, googleProvider e db)
 export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
+export const db = getFirestore(app);
+
+export default app;
