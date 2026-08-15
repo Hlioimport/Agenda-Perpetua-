a58755bd4463094173b9e6fc70e88669f6618805
Hlioimport <helio.pedro.98@gmail.com>
2026-08-15 20:54:28 -0300

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,22 +1,16 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBwrOl3IFveoD6MXUZKHC_0sa0XohvL_Ds",
+  apiKey: "AIzaSyBwr0L3IFveoD6MXUZKHC_0sa0XohvL...", // Sua chave completa
   authDomain: "agenda-perpetua.firebaseapp.com",
   projectId: "agenda-perpetua",
   storageBucket: "agenda-perpetua.firebasestorage.app",
   messagingSenderId: "1062998494461",
-  appId: "1:1062998494461:web:d817cfd39ebbd2b3450294"
+  appId: "1:1062998494461:web:d817cfd39ebbd2b3..."
 };
 
-// Inicializa o app Firebase
 const app = initializeApp(firebaseConfig);
 
-// Exporta todos os módulos que o projeto precisa (auth, googleProvider e db)
 export const auth = getAuth(app);
 export const googleProvider = new GoogleAuthProvider();
-export const db = getFirestore(app);
-
-export default app;
