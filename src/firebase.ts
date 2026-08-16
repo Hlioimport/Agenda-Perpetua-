dcf36db4297540a40091ea405daafbaac864f0e4
Hlioimport <helio.pedro.98@gmail.com>
2026-08-15 22:20:50 -0300

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "SUA_API_KEY_AQUI",
-  authDomain: "seu-projeto.firebaseapp.com",
-  projectId: "seu-projeto",
-  storageBucket: "seu-projeto.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "1:123456789:web:abcdef"
+  apiKey: "AIzaSyBwr0L3IFveoD6MXUZKHC_0sa0XohvL_Ds", 
+  authDomain: "agenda-perpetua.firebaseapp.com",
+  projectId: "agenda-perpetua",
+  storageBucket: "agenda-perpetua.firebasestorage.app",
+  messagingSenderId: "1062998494461",
+  appId: "1:1062998494461:web:d817cfd39ebbd2b345029"
 };
 
 // Inicializa o Firebase
